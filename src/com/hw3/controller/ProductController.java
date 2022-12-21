29e06412c2bac9995df6a36b10f6e873d7c54500
LEE JINYOUNG <jinijnleelee@gmail.com>
2022-12-21 15:18:35 +0900

    객체배열 실습문제2

--- src/com/hw3/controller/ProductController.java
+++ src/com/hw3/controller/ProductController.java
@@ -16,34 +16,53 @@ public class ProductController {
 	
 	
 	public void mainMenu() {
-		int inNum= 0;
+	int menuNum = 0;
+		
 		do {
-			System.out.println("=======제품 관리 메뉴======");			
-			System.out.println( "제품 정보 추가");
-			System.out.println("제품 정보 추가");
-			System.out.println("프로그램 종료");
-			
-			System.out.println("메뉴 입력>>>");
-			inNum = sc.nextInt();
+			System.out.println("==제품 관리 메뉴==");
+			System.out.println("1제품 정보 추가");
+			System.out.println("2제품 전체 조회");
+			System.out.println("0프로그램 종료");
+			menuNum = sc.nextInt();
 			sc.nextLine();
-		
-		
-		while(inNum) {
-			case 1: 
-				break;
-			case 2: 
-				break;
-			case 3: 
-				break;
+			
+			
+		switch(menuNum ) {
+		case 1 : 
+			productInput();
+			break;
+		case 2 :
+			productPrint();
+			break;
+		case 0 :System.out.println("종료");  
+		break;
 	
+
+		default : System.out.println("\n잘못 입력 하셨습니다");
 		}
 		
 		
-		
+		}while(menuNum !=0);
+			
 	}
 	
-	}
+	
+	
 	public void productInput() {
+		String bk = "";
+		System.out.println("==제품 관리 메뉴==");
+		bk = sc.next();
+		sc.nextLine();
+		
+		//키보드로 도서 정보를 입력 받아 객체 생성
+		//-> Product 생성자 내부에서 Product
+		for(int i= 0; i<pro.length;i++) {
+			if(pro[i]==null) {
+				pro[i]=
+				
+			}
+			
+		}
 		
 	}
 	
